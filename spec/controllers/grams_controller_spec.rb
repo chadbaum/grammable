c8ec4161fc020cfc8364e7f966633b80b43a3ed6
Chad Baum <chadbaum@gmail.com>
2016-06-30 17:05:22 +0000

    Added tests for gram#show and implemented action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,32 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+
   describe "grams#index action" do
+
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
   end
 
   describe "grams#new action" do
+
     it "should require users to be logged in" do
       get :new
       expect(response).to redirect_to new_user_session_path
     end
+
     it "should successfully show the new form" do
       user = FactoryGirl.create(:user)
       sign_in user
 
       get :new
       expect(response).to have_http_status(:success)
+
     end
   end
 
   describe "grams#create action" do
+
     it "should require users to be logged in" do
       post :create, gram: { message: 'Hello' }
       expect(response).to redirect_to new_user_session_path
     end
+
     it "should successfully create a new gram in our database" do
       user = FactoryGirl.create(:user)
       sign_in user
@@ -48,5 +56,22 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:unprocessable_entity)
       expect(gram_count).to eq Gram.count
     end
+
   end
+
+  describe "grams#show action" do
+
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :show, id: gram.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, id: 'TACOCAT'
+      expect(response).to have_http_status(:not_found)
+    end
+
+  end
+
 end
