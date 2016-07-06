d432cd0db815511179be04f1fe792e73291ec197
Chad Baum <chadbaum@gmail.com>
2016-07-06 21:13:17 +0000

    Added comment model, created tests for #create action and implemented and refactored code

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+
+    @gram = Gram.find_by_id(params[:gram_id])
+    return render_not_found if @gram.blank?
+
+    @comment = @gram.comments.create(comment_params)
+    if @comment.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
+
+
+
+end
